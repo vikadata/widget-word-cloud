bf4167b728678d5c8f1866415aff9ad0c6f738d7
Niko030303 <y1257891258@qq.com>
2021-12-22 17:08:03 +0800

    增加词云组件

--- legacy.d.ts
+++ legacy.d.ts
@@ -1,2 +1,3 @@
 declare module 'segmentit';
 declare module 'wordcloud2';
+declare module 'react-wordcloud2';
--- src/damu_arr.tsx
+++ src/damu_arr.tsx
@@ -1,18 +1,21 @@
-import $ from 'jquery'; 
 import React, {useRef, useState} from 'react';
 import { useField, FieldPicker, useActiveViewId, useCloudStorage, useActiveCell, useRecord} from '@vikadata/widget-sdk';
 import { Segment, useDefault } from 'segmentit';
-import { WordCloud } from 'wordcloud2';
+// import wordcloud2 from 'wordcloud2';
+import ReactWordcloud from 'react-wordcloud';
 import Axios from 'axios';
 
 
+
 export const DanmuArr: React.FC = () => {
-    
+    // const { WordCloud } = wordcloud2
+
     const viewId = useActiveViewId();
     const [fieldId, setFieldId] = useCloudStorage<string>('selectFieldId');
     const [txtData, setTxtData] = useState<null | any>() // 存放弹幕数组
+    const [info, setInfo] = useState<any>() // 存放需要渲染的词云字典
 
-    const myCanvas = useRef(null);
+    // const myCanvas = useRef(null);
 
     const currentField = useField(fieldId)
     const activeCell = useActiveCell(); // 获取当前光标激活的单元格坐标
@@ -30,16 +33,21 @@ export const DanmuArr: React.FC = () => {
         console.log("此时的txt文本:",txtData)       
     },[txtData])
 
+    React.useEffect(() => {
+        console.log("此时的info:",info)       
+    },[info])
+
     const GetList = () => {
         console.log('------')
         console.log("类型是:", currentField?.type)
+        //如果选择了附件列，就解析里面的txt文件，如果是其他类型的列，就提示‘所选单元格非附件类型’
         if(currentField?.type !== 'Attachment'){
             console.log('所选单元格非附件类型')
         }else{
             console.log("内容是: ", word_cloud_txt)
             console.log("文件名是: ", word_cloud_txt ? word_cloud_txt[0]['name'] : null)
-            
             console.log("文件地址: ", word_cloud_txt ? word_cloud_txt[0]['url'] : null)
+            
             // 放送axios网络请求，解析附件数据
             if (word_cloud_txt){
                 Axios.get(word_cloud_txt[0]['url'])
@@ -58,6 +66,8 @@ export const DanmuArr: React.FC = () => {
 
     const GetWordCloud = () => {
         var new_arr = []
+        var obj = {}
+        var final = []
         const segmentit = useDefault(new Segment());
         txtData?.forEach(item => {
             const result = segmentit.doSegment(item, {
@@ -66,12 +76,30 @@ export const DanmuArr: React.FC = () => {
            
             new_arr = [...new_arr, ...result]
         })
-        // const new_text = txtData?.toString()
-        // const result = segmentit.doSegment(new_text, {
-        //             stripPunctuation: true});
+
+        new_arr.forEach(item => {
+
+            if(obj[item]){
+                obj[item] += 1
+            }else{
+                obj[item] = 1
+            }
+
+
+        })
+        // console.log(obj);
+        for(var i in obj){
+            final = [...final, {
+                text: i,
+                value: obj[i]
+            }]
+        }
+        // let info = Object.entries(obj)
         
-        console.log(new_arr);
-        // $.WordCloud(myCanvas, { list: new_arr} );
+
+        console.log(final)
+        setInfo(final)
+
     }
 
     return (
@@ -81,7 +109,8 @@ export const DanmuArr: React.FC = () => {
             <p>---------</p>
             <button onClick={GetWordCloud}>Click</button>
             <p>---------</p>
-            <img ref={myCanvas}></img>
+            <ReactWordcloud words={info} />
+
         </div>
     );
 };
